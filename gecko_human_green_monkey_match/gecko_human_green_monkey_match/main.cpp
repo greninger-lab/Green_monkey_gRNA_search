d38568da82002610a124e54ae6a2546ed0dde659
Vikas Peddu <Vpeddu@uw.edu>
2019-08-06 12:28:51 -0700

    remade 20mcombined.txt

--- gecko_human_green_monkey_match/gecko_human_green_monkey_match/main.cpp
+++ gecko_human_green_monkey_match/gecko_human_green_monkey_match/main.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 #include <cstring>
 #include <algorithm>
+#include <fstream>
+
+using namespace std;
 
 struct suffix
 {
@@ -20,9 +23,47 @@ int cmp(struct suffix a, struct suffix b)
     return strcmp(a.suff, b.suff) < 0? 1 : 0;
 }
 
+int *buildSuffixArray(char *txt, int n)
+{
+    // A structure to store suffixes and their indexes
+    struct suffix suffixes[n];
+    
+    // Store suffixes and their indexes in an array of structures.
+    // The structure is needed to sort the suffixes alphabatically
+    // and maintain their old indexes while sorting
+    for (int i = 0; i < n; i++)
+    {
+        suffixes[i].index = i;
+        suffixes[i].suff = (txt+i);
+    }
+    
+    // Sort the suffixes using the comparison function
+    // defined above.
+    sort(suffixes, suffixes+n, cmp);
+    
+    // Store indexes of all sorted suffixes in the suffix array
+    int *suffixArr = new int[n];
+    for (int i = 0; i < n; i++)
+        suffixArr[i] = suffixes[i].index;
+    
+    // Return the suffix array
+    return  suffixArr;
+}
+
+void printArr(int arr[], int n)
+{
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream green_monkey_fasta;
+    green_monkey_fasta.open ("/Users/gerbix/Documents/vikas/gecko_green_monkey/green_monkey_n_removed.fasta");
+    
+    ifstream gRNAs;
+    gRNAs.open("/Users/gerbix/Documents/vikas/gecko_green_monkey/geck_20m_sequences_combined.fasta");
+    
+    cout << ;
     return 0;
 }
