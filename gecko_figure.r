b6e649905730fbce4d7837781378a730b283973b
Vikas Peddu <Vpeddu@uw.edu>
2019-08-07 11:49:20 -0700

    new graphs by coverage and included original unfiltered gecko too

--- gecko_figure.r
+++ gecko_figure.r
@@ -1,6 +1,7 @@
 library(ggplot2)
 setwd('/Users/gerbix/Documents/vikas/gecko_green_monkey')
 
+#gecko with pam
 gecko_a_with_pam<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_a_with_pam.csv')
 gecko_b_with_pam<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_b_with_pam.csv')
 
@@ -12,16 +13,32 @@ with_pam_genes_frequency$type[mirna]<-'miRNA'
 with_pam_genes_frequency$pam<-'Exact match with PAM'
 colnames(with_pam_genes_frequency)<-c('gene','freq','type','pam')
 
-
+#gecko exact matches without pam
 exact_match_gene_counts<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/exact_match_gene_counts.txt', sep = ' ', header = FALSE)
 exact_match_gene_counts$type<-'RNA'
 exact_mirna<-which(grepl('mir',exact_match_gene_counts$V2))
 exact_match_gene_counts$type[exact_mirna]<-'miRNA'
 exact_match_gene_counts$pam<-'Exact match'
-colnames(exact_match_gene_counts)<-c('freq','gene','type','pam')
+colnames(exact_match_gene_counts)<-c('gene','freq','type','pam')
 exact_match_gene_counts<-exact_match_gene_counts[,c(2,1,3,4)]
 
-combined<-rbind(exact_match_gene_counts,with_pam_genes_frequency)
+#gecko original
+gecko_a_original<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_a_original.csv')
+gecko_a_original$UID<-NULL
+gecko_a_original$seq<-NULL
+gecko_b_original<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_b_original.csv')
+gecko_b_original$UID<-NULL
+gecko_b_original$seq<-NULL
+gecko_original_combined<-data.frame(table(c(as.character(gecko_a_original$gene_id), as.character(gecko_b_original$gene_id))))
+gecko_original_mir<-which(grepl('mir',gecko_original_combined$Var1))
+gecko_original_combined$type<-'RNA'
+gecko_original_combined$type[gecko_original_mir]<-'miRNA'
+gecko_original_combined$pam<-'original'
+colnames(gecko_original_combined)<-c('gene','freq','type','pam')
+
+
+
+combined<-rbind(exact_match_gene_counts,with_pam_genes_frequency,gecko_original_combined)
 
 
 pam_RNA_vs_miRNA<-ggplot(with_pam_genes_frequency, aes( x = with_pam_genes_frequency$type)) + 
