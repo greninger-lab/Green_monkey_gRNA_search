ece16235e4724c3833820f30dfe2830d494a195e
Vikas Peddu <Vpeddu@uw.edu>
2019-08-14 10:29:09 -0700

    brunello figures

--- brunello/gecko_brunello_figure.r
+++ brunello/gecko_brunello_figure.r
@@ -12,56 +12,57 @@ myBreaks <- function(x){
 
 
 
-#gecko original
-gecko_a_original<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_a_original.csv')
-gecko_a_original$UID<-NULL
-gecko_a_original$seq<-NULL
-gecko_b_original<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_b_original.csv')
-gecko_b_original$UID<-NULL
-gecko_b_original$seq<-NULL
-gecko_original_combined<-data.frame(table(c(as.character(gecko_a_original$gene_id), as.character(gecko_b_original$gene_id))))
-gecko_original_mir<-which(grepl('mir',gecko_original_combined$Var1))
-gecko_original_combined$type<-'RNA'
-gecko_original_combined$type[gecko_original_mir]<-'miRNA'
-gecko_original_combined$pam<-'Original'
-colnames(gecko_original_combined)<-c('gene','freq','type','pam')
-gecko_original_unique<-unique(gecko_original_combined$gene)
+#brunello original
+brunello_original<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/brunello/brunello.csv', header = FALSE)
+brunello_original$UID<-NULL
+brunello_original$seq<-NULL
+#brunello_b_original<-read.csv('/Users/gerbix/Documents/vikas/_green_monkey/_b_original.csv')
+#_b_original$UID<-NULL
+#_b_original$seq<-NULL
+brunello_original_combined<-data.frame(table(c(as.character(brunello_original$V1))))
+brunello_original_mir<-which(grepl('MIR',brunello_original_combined$Var1))
+brunello_original_combined$type<-'RNA'
+brunello_original_combined$type[brunello_original_mir]<-'miRNA'
+brunello_original_combined$pam<-'Original'
+colnames(brunello_original_combined)<-c('gene','freq','type','pam')
+brunello_original_unique<-unique(brunello_original_combined$gene)
 
 #gecko with pam
-gecko_a_with_pam<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_a_with_pam.csv')
-gecko_b_with_pam<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_b_with_pam.csv')
+brunello_a_with_pam<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/brunello/gm_brunello_with_pam.csv')
+#gecko_b_with_pam<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_b_with_pam.csv')
 
-with_pam_genes_frequency<-data.frame(table(c(as.character(gecko_a_with_pam$with_pam_names), as.character(gecko_b_with_pam$with_pam_names))))
+with_pam_genes_frequency<-data.frame(table(c(as.character(brunello_a_with_pam$with_pam_names))))
 #adding in any 0s (anything in the original that isn't in this list)
-with_pam_genes_0<-data.frame(gecko_original_unique[which(as.character(gecko_original_unique) %!in% unique(as.character(with_pam_genes_frequency$Var1)))])
+with_pam_genes_0<-data.frame(brunello_original_unique[which(as.character(brunello_original_unique) %!in% unique(as.character(with_pam_genes_frequency$Var1)))])
 colnames(with_pam_genes_0)[1]<-'Var1'
 with_pam_genes_0$Freq<-0
 with_pam_genes_frequency<-rbind(with_pam_genes_frequency,with_pam_genes_0)
 
-mirna<-which(grepl('mir',with_pam_genes_frequency$Var1))
+mirna<-which(grepl('MIR',with_pam_genes_frequency$Var1))
 with_pam_genes_frequency$type<-'RNA'
 with_pam_genes_frequency$type[mirna]<-'miRNA'
 with_pam_genes_frequency$pam<-'Matched + PAM'
 colnames(with_pam_genes_frequency)<-c('gene','freq','type','pam')
 
-#gecko exact matches without pam
-exact_match_gene_counts<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/exact_match_gene_counts.txt', sep = ' ', header = FALSE)
-
+#brunello exact matches without pam
+exact_match_gene_counts<-read.csv('/Users/gerbix/Documents/vikas/gecko_green_monkey/brunello/brunello_exact_match_gene_counts.txt', sep = ' ', header = FALSE)
+exact_match_gene_counts<-exact_match_gene_counts[,c(4,5)]
+colnames(exact_match_gene_counts)<-c('V1','V2')
 #adding in any 0s (anything in the original that isn't in this list)
-exact_genes_0<-data.frame(gecko_original_unique[which(as.character(gecko_original_unique) %!in% unique(as.character(exact_match_gene_counts$V2)))])
+exact_genes_0<-data.frame(brunello_original_unique[which(as.character(brunello_original_unique) %!in% unique(as.character(exact_match_gene_counts$V2)))])
 colnames(exact_genes_0)[1]<-'V2'
 exact_genes_0$V1<-0
 exact_match_gene_counts<-rbind(exact_match_gene_counts,exact_genes_0)
 
 exact_match_gene_counts$type<-'RNA'
-exact_mirna<-which(grepl('mir',exact_match_gene_counts$V2))
+exact_mirna<-which(grepl('MIR',exact_match_gene_counts$V2))
 exact_match_gene_counts$type[exact_mirna]<-'miRNA'
 exact_match_gene_counts$pam<-'Matched'
 colnames(exact_match_gene_counts)<-c('freq','gene','type','pam')
 exact_match_gene_counts<-exact_match_gene_counts[,c(2,1,3,4)]
 
 
-combined<-rbind(gecko_original_combined,with_pam_genes_frequency,exact_match_gene_counts)
+combined<-rbind(brunello_original_combined,with_pam_genes_frequency,exact_match_gene_counts)
 combined$pam<-as.factor(combined$pam)
 
 pam_RNA_vs_miRNA<-ggplot(with_pam_genes_frequency, aes( x = with_pam_genes_frequency$type)) + 
@@ -108,7 +109,7 @@ ggsave(plot = pam_vs_not,'frequencies_by_sample_no_miRNA.pdf', height =3, width 
 
 
 
-gecko_origina_nomirna<-ggplot(combined_no_mirna, aes( x = pam, y = 1)) + 
+brunello_origina_nomirna<-ggplot(combined_no_mirna, aes( x = pam, y = 1)) + 
   geom_bar(stat = "identity", aes(fill = as.factor(freq))) + 
   #geom_text(aes(label=freq, y = freq), size=3) + 
   theme_classic() + 
@@ -121,12 +122,14 @@ gecko_origina_nomirna<-ggplot(combined_no_mirna, aes( x = pam, y = 1)) +
   scale_y_continuous(breaks = c(0,5500,11000,16500,22000), limits = c(0,22000)) + 
   xlab(NULL) + 
   ylab('Counts')
-gecko_origina_nomirna
-ggsave(plot = gecko_origina_nomirna,'no_mirna_human_gm.pdf', height =3, width = 3)
+brunello_origina_nomirna
+ggsave(plot = brunello_origina_nomirna,'no_mirna_human_gm.pdf', height =3, width = 3)
 
-combined<-combined[combined$freq!=8,]
+#deletes NTC
+combined<-combined[combined$freq<9,]
 
-gecko_original<-ggplot(combined, aes( x = pam, y = 1))  + 
+
+brunello_original<-ggplot(combined, aes( x = pam, y = 1))  + 
   geom_bar(stat = "identity", aes(fill = as.factor(freq))) + 
   #geom_text(aes(label=freq, y = freq), size=3) + 
   theme_classic() + 
@@ -138,8 +141,8 @@ gecko_original<-ggplot(combined, aes( x = pam, y = 1))  +
   scale_y_continuous(breaks = c(0,5500,11000,16500,22000), limits = c(0,22000)) + 
   xlab(NULL) + 
   ylab('Counts')
-gecko_original
-ggsave(plot = gecko_original,'human_gm.pdf', height =3, width = 3)
+brunello_original
+ggsave(plot = brunello_original,'brunello_gm.pdf', height =3, width = 3)
 
 
 
@@ -147,9 +150,9 @@ sum(combined_no_mirna$freq[combined_no_mirna$pam=='Matched']) + length(combined_
 sum(combined_no_mirna$freq[combined_no_mirna$pam=='Matched + PAM']) + length(combined_no_mirna$freq[combined_no_mirna$pam=='Matched + PAM' & combined_no_mirna$freq==0]) 
 
 
-summary_table<-t(data.frame(table(gecko_original_combined$freq)))
+summary_table<-data.frame(t(data.frame(table(brunello_original_combined$freq[brunello_original_combined$freq < 9]))))
 summary_table<-rbind(summary_table,t(data.frame(table(exact_match_gene_counts$freq))[2]))
-summary_table<-rbind(summary_table,t(data.frame(table(with_pam_genes_frequency$freq))[2]))
+summary_table<-data.frame(rbind(summary_table,t(data.frame(table(with_pam_genes_frequency$freq))[2])))
 rownames(summary_table)<-c('matches','original','matched','matched with pam')
 
 
--- brunello/gecko_brunello_match.r
+++ brunello/gecko_brunello_match.r
@@ -4,8 +4,8 @@ library(svMisc)
 
 setwd('/Users/gerbix/Documents/vikas/gecko_green_monkey/brunello')
 
-#gecko_brunello<-scanBam('/Users/gerbix/Documents/vikas/gecko_green_monkey/gecko_brunello_20m.bam')
-gecko_brunello<-scanBam('/Users/gerbix/Documents/vikas/gecko_green_monkey/brunello/brunello.bam')
+#gm_brunello<-scanBam('/Users/gerbix/Documents/vikas/gecko_green_monkey/gm_brunello_20m.bam')
+gm_brunello<-scanBam('/Users/gerbix/Documents/vikas/gecko_green_monkey/brunello/brunello.bam')
 
 fasta_list<-list.files('/Users/gerbix/Documents/vikas/gecko_green_monkey/green_monkey_fastas', pattern = '*.fa$', full.names = TRUE)
 
@@ -22,44 +22,44 @@ with_pam_names_rc<-c()
 with_pam_seq_rc<-c()
 without_pam_seq<-c()
 without_pam_name<-c()
-for (i in 1:length(gecko_brunello[[1]]$rname)){ 
+for (i in 1:length(gm_brunello[[1]]$rname)){ 
   progress(i,65596)
-  temp<-which( dna_names == gecko_brunello[[1]]$rname[i])
+  temp<-which( dna_names == gm_brunello[[1]]$rname[i])
   #temp_dna_seq is the dna string from the reference
-  temp_dna_seq<-as.character(substr(dna[temp,], gecko_brunello[[1]]$pos[i], (gecko_brunello[[1]]$pos[i] + 22)))
+  temp_dna_seq<-as.character(substr(dna[temp,], gm_brunello[[1]]$pos[i], (gm_brunello[[1]]$pos[i] + 22)))
   #revcomp is the reverse complement dna string from the reference
-  revcomp<-as.character(as.character(substr(dna[temp,], gecko_brunello[[1]]$pos[i] -3 , (gecko_brunello[[1]]$pos[i] + 20))))
+  revcomp<-as.character(as.character(substr(dna[temp,], gm_brunello[[1]]$pos[i] -3 , (gm_brunello[[1]]$pos[i] + 20))))
   # print(temp_dna_seq)
   #  print((revcomp))
   # 
-  # temp_a<-paste0(gecko_brunello[[1]]$seq[i], 'AGG')
-  # temp_c<-paste0(gecko_brunello[[1]]$seq[i], 'CGG')
-  # temp_t<-paste0(gecko_brunello[[1]]$seq[i], 'TGG')
-  # temp_g<-paste0(gecko_brunello[[1]]$seq[i], 'GGG')
+  # temp_a<-paste0(gm_brunello[[1]]$seq[i], 'AGG')
+  # temp_c<-paste0(gm_brunello[[1]]$seq[i], 'CGG')
+  # temp_t<-paste0(gm_brunello[[1]]$seq[i], 'TGG')
+  # temp_g<-paste0(gm_brunello[[1]]$seq[i], 'GGG')
   # 
-  temp_nuc<-paste0(gecko_brunello[[1]]$seq[i], '.GG')
-  # temp_a_rev<-paste0('CCT',gecko_brunello[[1]]$seq[i])
-  # temp_c_rev<-paste0('CCG',gecko_brunello[[1]]$seq[i])
-  # temp_t_rev<-paste0('CCC',gecko_brunello[[1]]$seq[i])
-  # temp_g_rev<-paste0('CCA',gecko_brunello[[1]]$seq[i])
-  temp_nuc_rev<-paste0('CC.',gecko_brunello[[1]]$seq[i])
+  temp_nuc<-paste0(gm_brunello[[1]]$seq[i], '.GG')
+  # temp_a_rev<-paste0('CCT',gm_brunello[[1]]$seq[i])
+  # temp_c_rev<-paste0('CCG',gm_brunello[[1]]$seq[i])
+  # temp_t_rev<-paste0('CCC',gm_brunello[[1]]$seq[i])
+  # temp_g_rev<-paste0('CCA',gm_brunello[[1]]$seq[i])
+  temp_nuc_rev<-paste0('CC.',gm_brunello[[1]]$seq[i])
   
   #if(grepl(temp_a,temp_dna_seq) |grepl(temp_c,temp_dna_seq) | grepl(temp_t,temp_dna_seq) | grepl(temp_g,temp_dna_seq)){ 
   if(grepl(temp_nuc,temp_dna_seq)){
     #print('ok')
-    with_pam_names<-append(with_pam_names, gecko_brunello[[1]]$qname[i])
+    with_pam_names<-append(with_pam_names, gm_brunello[[1]]$qname[i])
     with_pam_seq<-append(with_pam_seq, temp_dna_seq)
   }
   else if(grepl(temp_nuc_rev,revcomp)){ 
     #print('rc')
-    with_pam_names<-append(with_pam_names, gecko_brunello[[1]]$qname[i])
+    with_pam_names<-append(with_pam_names, gm_brunello[[1]]$qname[i])
     with_pam_seq<-append(with_pam_seq, temp_dna_seq)
   }
   else{ 
     # print(temp_dna_seq)
     # print(revcomp)
     # print('')
-    without_pam_name<-append(without_pam_name, gecko_brunello[[1]]$qname[i])
+    without_pam_name<-append(without_pam_name, gm_brunello[[1]]$qname[i])
     without_pam_seq<-append(without_pam_seq, revcomp)
   }
 }
